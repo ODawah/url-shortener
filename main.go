5552e1240d336365b8c7152bacdb7793638c2a6b
ODawah <omardawah550@gmail.com>
2024-04-15 08:56:20 +0200

    handled the containers starting sequence from main function

--- main.go
+++ main.go
@@ -7,17 +7,39 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	err := persistence.InitializeSQL()
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
+	ticker := time.NewTicker(10 * time.Second)
+	quit := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				err = persistence.InitializeSQL()
+				if err != nil {
+					log.Println(err)
+				}
+
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello, World!"))
 	})
+	log.Println("up and running")
 	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
