08daf438f1a2f1fe52dec193d7ff4d905f240640
ODawah <omardawah550@gmail.com>
2024-04-18 13:34:17 +0200

    added url validation function

--- models/urls.go
+++ models/urls.go
@@ -1,9 +1,12 @@
 package models
 
 import (
+	"errors"
 	"fmt"
 	"github.com/ODawah/url-shortener/encoders"
 	"gorm.io/gorm"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -13,33 +16,40 @@ type URL struct {
 	ID       uint `gorm:"primaryKey"`
 	Original string
 	Short    string `gorm:"UNIQUE"`
-	User     User   `gorm:"foreignkey:UserID" json:"-"`
+	User     User   `gorm:"foreignKey:UserID" json:"-"`
 }
 
-func (url *URL) CreateURL(db *gorm.DB) error {
-	err := url.EncodeURL()
+func (u *URL) CreateURL(db *gorm.DB) error {
+	err := u.ValidateOriginalURL()
 	if err != nil {
 		return err
 	}
-	err = db.Create(&url).Error
+	err = u.EncodeURL()
 	if err != nil {
+		return err
+	}
+	err = db.Create(&u).Error
+	if err != nil {
+		if err.Error() == gorm.ErrDuplicatedKey.Error() {
+			return errors.New("Duplicated KEY")
+		}
 		return err
 	}
 	return nil
 }
 
 func GetOriginalURL(shortURL string, db *gorm.DB) (URL, error) {
-	var url URL
-	if err := db.Where("short = ?", shortURL).First(&url).Error; err != nil {
-		return url, err
+	var u URL
+	if err := db.Where("short = ?", shortURL).First(&u).Error; err != nil {
+		return u, err
 	}
-	return url, nil
+	return u, nil
 }
 
-func (url *URL) EncodeURL() error {
+func (u *URL) EncodeURL() error {
 	currentTime := time.Now()
 	currentTimeString := currentTime.Format("2006-01-02 15:04:05")
-	md5encode, err := encoders.MD5Encode(fmt.Sprintf("%s%s", url.Original, currentTimeString))
+	md5encode, err := encoders.MD5Encode(fmt.Sprintf("%s%s", u.Original, currentTimeString))
 	if err != nil {
 		return err
 	}
@@ -48,6 +58,18 @@ func (url *URL) EncodeURL() error {
 		return err
 	}
 	result := base62encode[:7]
-	url.Short = result
+	u.Short = result
 	return nil
+}
+
+func (u *URL) ValidateOriginalURL() error {
+	_, err := url.Parse(u.Original)
+	if err != nil {
+		return err
+	}
+	if strings.HasPrefix(u.Original, "https://") || strings.HasPrefix(u.Original, "http://") {
+		return errors.New(`the url should start with "http://" or "https://" `)
+	}
+
+	return err
 }
